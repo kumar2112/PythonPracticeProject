b70f067e168b1be936a74b33fc270bb6c998f7a1
Praveen Kumar Jha <codewithkumar@gmail.com>
2025-03-07 08:15:25 +0530

    Add Key Press Event

--- WordGuessingGame/GameUi.py
+++ WordGuessingGame/GameUi.py
@@ -16,30 +16,27 @@ userv = StringVar()
 user = Entry(app, textvariable=userv, justify=CENTER, relief=FLAT, borderwidth=2, font='Helvicta 18 bold')
 user.pack(pady=10)
 
-displayLable = ' * '*len(word)
-sc = Label(app, text=displayLable, font='lucida 8 bold ').pack( 
-      anchor=E, padx=200, pady=5) 
+n = len(word)
+repeated_list = ['*'] * n
+displayLable = "".join(repeated_list)
+sc = Label(app, text=displayLable, font='lucida 8 bold ')
+sc.pack(anchor=E, padx=200, pady=5) 
+
+k =n
 
 def on_key_pressed(event):
+   global k
    entered_char = userv.get()
-
-    # Update the label with the entered character
-   sc.config(text=f"Character Entered: {entered_char}")
-   # print("Char Entered")
-   # # char = userv.get().lower()
-   # # if char in word:
-   # #    new_display = list(displayLable)
-   # #    for i, c in enumerate(word):
-   # #       if c == char:
-   # #          new_display[i * 3 + 1] = char
-   # #    displayLable = ''.join(new_display)
-   # #    sc.config(text=displayLable)
-   # # else:
-   # #    count += 1
-      
-   # # userv.set('')
-
-
+   
+   if entered_char in word :
+        repeated_list[n-k]=entered_char
+        k -=1
+        print(k)
+        
+        
+        sc.config(text="".join(repeated_list))
+   
+user.bind('<KeyPress>',on_key_pressed)
 
 
 
@@ -52,13 +49,7 @@ def gameBoard() :
    app.maxsize(600, 600) 
    
    heading = Label(text='Word Guessing Game', font="Helvicta 16 bold", bg='red', fg='green', padx=170).pack() 
-   
-   
- 
-   
-   
-   footer = Label(text=f'Developed by Praveen Kumar Jha',  font="Helvicta 8 bold", 
-               bg='black', fg='tomato', padx=153).pack(side=BOTTOM)
+   footer = Label(text=f'Developed by Praveen Kumar Jha',  font="Helvicta 8 bold", bg='black', fg='tomato', padx=153).pack(side=BOTTOM)
  
   
 
